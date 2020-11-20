4cfe7ad4ce262971856431a65bb7d9c9eda99e7f
alextogar <smirnov.togar@ya.ru>
2020-11-20 13:38:32 +0300

    little fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import "./App.css";
-import Navbar from "./components/Navbar.js";
-import { Switch, Route, BrowserRouter, HashRouter } from "react-router-dom";
-import Home from "./components/pages/HomePage/Home.js";
-import Footer from "./components/pages/Footer/Footer.js";
-import Services from "./components/pages/Services/Services.js";
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/Navbar.js';
+import { Switch, Route, BrowserRouter, HashRouter } from 'react-router-dom';
+import Home from './components/pages/HomePage/Home.js';
+import Footer from './components/pages/Footer/Footer.js';
+import Services from './components/pages/Services/Services.js';
 
 class App extends Component {
   render() {
@@ -12,8 +12,8 @@ class App extends Component {
       <HashRouter>
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/services" exact component={Services} />
+          <Route path='/' exact component={Home} />
+          <Route path='/services' exact component={Services} />
         </Switch>
         <Footer />
       </HashRouter>
